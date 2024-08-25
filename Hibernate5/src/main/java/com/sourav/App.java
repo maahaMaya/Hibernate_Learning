fed0ec2fdbb82259575a542b24288e814adb3405
sourav <devbhsourav@gmail.com>
2024-08-25 20:36:20 +0530

    [sourav]add : File_Configuration_Connection

--- Hibernate5/src/main/java/com/sourav/App.java
+++ Hibernate5/src/main/java/com/sourav/App.java
@@ -1,4 +1,4 @@
-package com.sourav.Hibernate5;
+package com.sourav;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,12 +11,17 @@ public class App
 {
     public static void main( String[] args )
     {
+    	//XML Configuration
     	//Configuration configuration = new Configuration();
     	//configuration.configure(); // for reading hibernate.cfg.xml file
     	//SessionFactory sessionFactory = configuration.buildSessionFactory(); // read each property/information of the hibernate.cfg.xml file 
         
-    	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
     	//SessionFactory sessionFactory = new Configuration().configure("AnyName(hibernate).xml").buildSessionFactory(); // config file change
-    	System.out.println( "SessionFactoryObject ::  " + sessionFactory );
+    	//SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    	//System.out.println( "XML Config -> SessionFactoryObject ::  " + sessionFactory );
+    	
+    	//File Configuration
+    	HibernateUtil hibernateUtilFileConfig = new HibernateUtil();
+    	hibernateUtilFileConfig.getSessionFactory();
     }
 }
---
+++ Hibernate5/src/main/java/com/sourav/HibernateUtil.java
@@ -0,0 +1,44 @@
+package com.sourav;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
+				
+				Properties properties = new Properties();
+				properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+				properties.put(Environment.URL, "jdbc:mysql://localhost:3306/gfive");
+				properties.put(Environment.USER, "root");
+				properties.put(Environment.PASS, "12345");
+				properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+				properties.put(Environment.HBM2DDL_AUTO, "update");
+				properties.put(Environment.SHOW_SQL, "true");
+				
+				configuration.setProperties(properties);
+				
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+				
+				sessionFactory =  configuration.buildSessionFactory(serviceRegistry);
+				
+				System.out.println( "File Conig -> SessionFactoryObject ::  " + sessionFactory );
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sessionFactory;
+	}
+}
