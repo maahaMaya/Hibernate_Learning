7230418b069609a6a15090f0948231c8f213aadc
sourav <devbhsourav@gmail.com>
2024-08-27 23:43:14 +0530

    [sourav]add : Employee_Entity

---
+++ Hibernate5/src/main/java/com/sourav/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.sourav.entity;
+
+import java.util.Date;
+
+public class Employee {
+	
+	private int id;
+	
+	private String empName;
+	
+	private Double salary;
+	
+	private Date joiningDate;
+	
+	private boolean status;
+	
+	private String token;
+}
